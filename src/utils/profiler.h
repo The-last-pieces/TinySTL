49988f1370c39b89ea59f1d9290fc18c02a261f1
MnZn <2776835685@qq.com>
2022-04-26 21:04:42 +0800

    feat: add task name arg

--- src/utils/profiler.h
+++ src/utils/profiler.h
@@ -13,16 +13,20 @@ namespace ttl {
      * 通用计时器
      */
     class auto_timer {
+        const char *const task_name;
         system_clock::time_point start;
 
     public:
         // start record when entering scope
-        auto_timer() : start(system_clock::now()) {}
+        explicit auto_timer(const char *task_name = nullptr) :
+                task_name(task_name), start(system_clock::now()) {
+        }
 
         // end record when leaving scope
         ~auto_timer() {
             auto cost = duration_cast<nanoseconds>(system_clock::now() - start);
-            std::cout << "Time cost: " << double(cost.count()) / 1000000.0 << " ms" << std::endl;
+            if (task_name) std::cout << task_name << ' ';
+            std::cout << "cost: " << double(cost.count()) / 1000000.0 << " ms" << std::endl;
         }
     };
 
