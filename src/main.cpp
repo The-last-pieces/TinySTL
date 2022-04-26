649fc70762e6369218ad2d8d77ac9455c9a162d6
MnZn <2776835685@qq.com>
2022-04-26 21:10:31 +0800

    test: add memory tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,30 @@
 #include "./utils/profiler.h"
+#include "./allocator/memory.h"
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
 
 using namespace std;
-using namespace tstd;
+using namespace ttl;
 
 // write all test code
 int main() {
     {
-        auto_timer timer;
-        cout << "hello world" << endl;
+        std::allocator<string> allocator;
+        auto *ptr = allocator.allocate(1000000);
+        {
+            auto_timer timer("stl");
+            std::uninitialized_fill(ptr, ptr + 1000000, "123");
+        }
+    }
+    {
+        ttl::allocator<string> allocator;
+        auto *ptr = allocator.allocate(1000000);
+        {
+            auto_timer timer("ttl");
+            ttl::uninitialized_fill(ptr, ptr + 1000000, "123");
+        }
     }
     return 0;
 }
