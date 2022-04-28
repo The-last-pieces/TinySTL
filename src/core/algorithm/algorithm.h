5fc714798d30528592784db071af5d44d3dfff1b
MnZn <2776835685@qq.com>
2022-04-28 23:45:10 +0800

    feat: add dict comparor

--- src/core/algorithm/algorithm.h
+++ src/core/algorithm/algorithm.h
@@ -68,6 +68,19 @@ namespace ttl {
     }
 
     /*
+     * 比较操作
+     */
+    // 字典序<
+    template<typename InputIt1, typename InputIt2>
+    bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2) {
+        for (; (first1 != last1) && (first2 != last2); ++first1, ++first2) {
+            if (*first1 < *first2) return true;
+            if (*first2 < *first1) return false;
+        }
+        return (first1 == last1) && (first2 != last2);
+    }
+
+    /*
      * 排列操作
      */
 }
