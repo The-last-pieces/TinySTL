3c4ae651a21a321a40a7922d463740742abec35b
MnZn <2776835685@qq.com>
2022-05-01 22:37:51 +0800

    test: add full test

--- src/tests/list_test.h
+++ src/tests/list_test.h
@@ -17,25 +17,125 @@ namespace ttl::ttl_test {
     public:
         static void runAll() {
             test1();
+            test2();
+            test3();
+            test4();
+            test5();
+            test6();
+            test7();
+            test8();
         }
 
     private:
         static void test1() {
+            auto rd = randArray<std::string>(
+                    10000,
+                    []() -> std::string { return randStr(10); });
             ttl::list<std::string> tl;
             std::list<std::string> sl;
             TTL_STL_COMPARE(tl, sl, {
-                v.emplace_front("hhh2");
-                v.emplace_front("hhh2");
-                v.emplace_front("hhh2");
-                v.assign(112300, "hello");
-                v.insert(-- -- --v.end(), 102420, "world");
+                v.assign(rd.begin(), rd.end());
+                v.insert(ttl::prev(v.end(), 3), 12420, "world");
                 v.emplace_back("hhh");
-                v.pop_front();
-                v.insert((++ ++ ++ ++ ++ ++v.rbegin()).base(), 13211, "asd");
-                v.pop_front();
-                v.erase(++ ++ ++v.begin(), -- -- --v.end());
-            }, "list insert & remove");
+                v.insert(ttl::prev(v.rend(), 3000).base(), 13211, "asd");
+            }, "list insert");
             assert(ttl::equal(tl.begin(), tl.end(), sl.begin(), sl.end()));
+        }
+
+        static void test2() {
+            auto rd = randArray<std::string>(
+                    10000,
+                    []() -> std::string { return randStr(10); });
+            ttl::list<std::string> tl;
+            std::list<std::string> sl;
+            TTL_STL_COMPARE(tl, sl, {
+                v.assign(rd.begin(), rd.end());
+                v.erase(ttl::prev(v.rend(), 100).base(), ttl::next(v.begin(), 5000));
+                v.erase(ttl::next(v.begin(), 1000), ttl::next(v.begin(), 3000));
+            }, "list erase");
+            assert(ttl::equal(tl.begin(), tl.end(), sl.begin(), sl.end()));
+        }
+
+        static void test3() {
+            std::list<std::string> sa;
+            ttl::list<std::string> ta;
+            TTL_STL_COMPARE(ta, sa, {
+                v.resize(10000, "12345");
+                v.resize(9900);
+            }, "list resize 1");
+            same(sa, ta);
+        }
+
+        static void test4() {
+            std::list<std::string> sa;
+            ttl::list<std::string> ta;
+            TTL_STL_COMPARE(ta, sa, {
+                v.resize(9900, "12345");
+                v.resize(1);
+            }, "list resize 2");
+            same(sa, ta);
+        }
+
+        static void test5() {
+            std::list<int> sa;
+            ttl::list<int> ta;
+            TTL_STL_COMPARE(ta, sa, {
+                v.assign({1, 3, 5, 7, 9});
+                v.merge({2, 4, 6, 8, 10});
+            }, "list merge 1");
+            same(sa, ta);
+            TTL_STL_COMPARE(ta, sa, {
+                v.assign({1, 2, 3});
+                v.merge({4, 5, 6});
+            }, "list merge 2");
+            same(sa, ta);
+        }
+
+        static void test6() {
+            auto rd = randArray<std::string>(
+                    100000,
+                    []() -> std::string { return randStr(10); });
+            std::list<std::string> sa(rd.begin(), rd.end());
+            ttl::list<std::string> ta(rd.begin(), rd.end());
+            TTL_STL_COMPARE(ta, sa, {
+                v.reverse();
+                v.reverse();
+                v.reverse();
+            }, "list reverse");
+            same(sa, ta);
+        }
+
+        static void test7() {
+            auto rd = randArray<std::string>(
+                    100000,
+                    []() -> std::string { return randStr(10); });
+
+            std::list<std::string> sa;
+            ttl::list<std::string> ta;
+            std::list<std::string> sa2(rd.begin(), rd.end());
+            ttl::list<std::string> ta2(rd.begin(), rd.end());
+            TTL_STL_COMPARE_2(
+                    {
+                        ta.splice(ta.end(), ta2, ttl::next(ta2.begin(), 500), ta2.end());
+                    },
+                    {
+                        sa.splice(sa.end(), sa2, ttl::next(sa2.begin(), 500), sa2.end());
+                    }, "list splice"
+            );
+            same(sa, ta);
+            same(sa2, ta2);
+        }
+
+        static void test8() {
+            auto rd = randArray<std::string>(
+                    100000,
+                    []() -> std::string { return randStr(randInt(10) + 1); });
+            std::list<std::string> sa(rd.begin(), rd.end());
+            ttl::list<std::string> ta(rd.begin(), rd.end());
+            TTL_STL_COMPARE(ta, sa, {
+                v.sort();
+            }, "list sort");
+            same(sa, ta);
         }
     };
 }
