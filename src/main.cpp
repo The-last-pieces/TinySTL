4efd98e4ca1ecdc721b202fa4cb148a9a88d6f10
MnZn <2776835685@qq.com>
2022-05-04 22:51:12 +0800

    test: add hashtable tests , set fix rand seed

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include "./tests/memory_test.h"
 #include "./tests/vector_test.h"
 #include "./tests/list_test.h"
+#include "./tests/hashtable_test.h"
 
 using namespace ttl::ttl_test;
 
 // write all test code
 int main() {
+    srand(reinterpret_cast<int *>(main) == (int *) (0x123)); // same as seed(0)
+    hashtable_test::runAll();
     list_test::runAll();
     vector_test::runAll();
     memory_test::runAll();
