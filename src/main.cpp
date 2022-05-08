683f9cb754d3890f59df22c7c2379932fa08beec
MnZn <2776835685@qq.com>
2022-05-08 19:33:11 +0800

    test: add <deque> module

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
-#include "./tests/memory_test.h"
+﻿#include "./tests/memory_test.h"
 #include "./tests/vector_test.h"
 #include "./tests/list_test.h"
 #include "./tests/hashtable_test.h"
 #include "./tests/unordered_map_test.h"
 #include "./tests/priority_queue_test.h"
 #include "./tests/bitset_test.h"
+#include "./tests/deque_test.h"
 
 using namespace ttl::ttl_test;
 
 // write all test code
 int main() {
+    deque_test::runAll();
     bitset_test::runAll();
     priority_queue_test::runAll();
     unordered_map_test::runAll();
