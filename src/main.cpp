6fb9590dcbf02ad98eb5c4d232d4cdbde26699b4
MnZn <2776835685@qq.com>
2022-07-06 23:01:55 +0800

    test: add test case

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,16 @@
 #include "./tests/priority_queue_test.h"
 #include "./tests/bitset_test.h"
 #include "./tests/deque_test.h"
+#include "./tests/bs_tree_test.h"
+#include "./tests/segment_tree_test.h"
 
 using namespace ttl::ttl_test;
 
 // write all test code
 int main() {
+    segment_tree_test::runAll();
+    // if (time(nullptr)) return 0;
+    bs_tree_test::runAll();
     deque_test::runAll();
     bitset_test::runAll();
     priority_queue_test::runAll();
